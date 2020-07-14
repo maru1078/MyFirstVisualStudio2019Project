79d7c242afde9c40d7765a7f398b5266b6c0d500
maru1078 <63898861+maru1078@users.noreply.github.com>
2020-07-14 12:55:37 +0900

    Add project

---
+++ MyFirstVS2019Project/MyFirstVS2019Project/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	std::string input;
+
+	std::cout << "プログラム開始" << std::endl;
+
+	while (input != "end")
+	{
+		// ゲームオブジェクトや、コンポーネントの処理
+
+		std::cin >> input;
+	}
+
+	std::cout << "プログラム終了" << std::endl;
+
+	return 0;
+}
